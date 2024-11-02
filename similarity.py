fix similarity computation

similarity.py
@@ -5,20 +5,25 @@ from torchvision import transforms
 from helpers.model import SimilarityCNN
 
 def loadModel(model_path):
+
     model = SimilarityCNN()
     model.load_state_dict(torch.load(model_path))
     model.eval()
+    
     return model
 
 def preprocessImage(image_path):
+
     transform = transforms.Compose([
         transforms.Resize((200, 200)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.5], std=[0.5])
     ])
+
     image = Image.open(image_path).convert('L')
     image = transform(image)
     image = image.unsqueeze(0)  # Add batch dimension, shape [1, 1, 200, 200]
+    
     return image
 
 def computeSimilarity(model, image1, image2):
@@ -28,16 +33,10 @@ def computeSimilarity(model, image1, image2):
         output1 = model(image1)
         output2 = model(image2)
 
-        output1 = F.normalize(output1, p=2, dim=1)
-        output2 = F.normalize(output2, p=2, dim=1)
-
-        cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-        similarity = cos(output1, output2)
-
-        # Force values to be between 0 (similar images) and 1 (different images)
-        similarity = (similarity + 1) / 2
-
-        return similarity.item()
+        distance = F.pairwise_distance(output1, output2)
+        similarity = 1 / (1 + distance.item())
+        
+        return similarity
 
 if __name__ == "__main__":
 

train_and_eval.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from helpers.preprocessing import loadData
 from helpers.model import SimilarityCNN
 import torch.nn as nn
-from sklearn.metrics import accuracy_score, precision_recall_fscore_support, roc_curve
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support, roc_curve, auc
 import numpy as np
 
 # Define contrastive loss for learning similar or dissimilar pairs
@@ -66,9 +66,6 @@ def trainModel(train_loader, model, criterion, optimizer, num_epochs=10):
             loss.backward()
             optimizer.step()
 
-            #print outputs for debugging
-            #print(f'Epoch {epoch+1}, Batch {i+1}, Output1: {outputs1.detach().cpu().numpy()}, Output2: {outputs2.detach().cpu().numpy()}')
-
             running_loss += loss.item()
             batch_count += 1
 
@@ -89,8 +86,7 @@ def evaluateModel(test_loader, model, criterion):
 
     test_loss = 0.0
 
-    all_outputs1 = []
-    all_outputs2 = []
+    all_outputs = []
     all_labels = []
 
     # Disable gradient calculation to evaluate model
@@ -114,24 +110,27 @@ def evaluateModel(test_loader, model, criterion):
             outputs2 = model(data2)
 
             loss = criterion(outputs1, outputs2, pair_labels)
-
             test_loss += loss.item()
 
-            all_outputs1.append(outputs1.cpu().numpy())
-            all_outputs2.append(outputs2.cpu().numpy())
+            distances = F.pairwise_distance(outputs1, outputs2)
+            
+            # Force outputs to be between 0 and 1
+            max_distance = distances.max()
+            similarities = 1 - distances / max_distance
+            
+            all_outputs.append(similarities.cpu().numpy())
             all_labels.append(pair_labels.cpu().numpy())
+
 
     test_loss /= len(test_loader)
     print(f'Test Loss: {test_loss:.3f}')
 
-    all_outputs1 = np.concatenate(all_outputs1)
-    all_outputs2 = np.concatenate(all_outputs2)
+    all_outputs = np.concatenate(all_outputs)
     all_labels = np.concatenate(all_labels)
 
-    # Evaluate distances and similarity based on a threshold 
-    distances = F.pairwise_distance(torch.tensor(all_outputs1), torch.tensor(all_outputs2)).numpy()
-    fpr, tpr, thresholds = roc_curve(all_labels, -distances)  # Minus sign for similarity
-    #roc_auc = auc(fpr, tpr)
+    # Evaluate similarity based on a threshold 
+    fpr, tpr, thresholds = roc_curve(all_labels, all_outputs)
+    roc_auc = auc(fpr, tpr) 
 
     # Youden's J statistic for optimal threshold
     optimal_idx = np.argmax(tpr + (1 - fpr) - 1)
@@ -140,11 +139,13 @@ def evaluateModel(test_loader, model, criterion):
     #import pdb 
     #pdb.set_trace()
 
-    predictions = (distances < -optimal_threshold).astype(int)
+    predictions = (all_outputs >= optimal_threshold).astype(int)
 
     accuracy = accuracy_score(all_labels, predictions)
     precision, recall, f1, _ = precision_recall_fscore_support(all_labels, predictions, average='binary')
 
+    print(f'AUC: {roc_auc:.3f}')
+    print(f'Optimal Threshold: {optimal_threshold:.3f}')
     print(f'Accuracy: {accuracy:.3f}')
     print(f'Precision: {precision:.3f}')
     print(f'Recall: {recall:.3f}')